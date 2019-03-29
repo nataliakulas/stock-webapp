18b75e877b0b25c3fed5d8c631bf11bb4a36baa0
Natalia Kulas <nataliakulas@gmail.com>
2019-03-29 10:33:43 +0100

    Basic styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,9 @@ import { Container, setConfiguration } from "react-grid-system";
 import styled, { createGlobalStyle } from "styled-components";
 import styledNormalize from "styled-normalize";
 
+import color from "../shared/colors";
+import background from "../assets/tiny-squares.png";
+
 const Layout = ({ children }) => (
   <Page>
     <GlobalStyle />
@@ -25,4 +28,22 @@ setConfiguration({
 
 const GlobalStyle = createGlobalStyle`
   ${styledNormalize};
+  
+    
+  * {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+    outline-color: ${color.secondary};
+  }
+  
+  body {
+    font-family: "Lato", sans-serif;
+    color: ${color.black};
+    background-image: url(${background});
+  }
+  
+  ::selection {
+    color: ${color.white};
+    background-color: ${color.secondary};
+  }
 `;
---
+++ src/shared/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  black: "#000E14",
+  blue: "#003049",
+  primary: "#E1CE7A",
+  secondary: "#F77F00",
+  cream: "#EAE2B7",
+  white: "#FFF",
+  red: "#D62828"
+};
+
+export default colors;
